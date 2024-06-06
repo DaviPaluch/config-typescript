20f00308ac277c684c76dc8de4f181195644a509
davi paluch <davipaluch@icloud.com>
2024-06-06 13:52:28 -0300

    projeto Node JS com Typescript. Debug configurado.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -9,11 +9,27 @@ const categories = [];
 categoriesRoutes.post("/", (request: Request, response: Response) => {
   const { name, description } = request.body
 
+  const category = new Category()
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date(),
+    id: 1 /* exemplo de id */
+  })
+  //Utilizamos o a função do JS -> Object; Otamos por uma escrita mais simples do a demonstrada:
+  /*
+  ex.01
   const category: Category = {
     name,
     description,
     created_at: new Date()
   }
+  ex.02
+  category.name=name;
+  category.description=description;
+
+  */
 
   categories.push(category)
   return response.status(201).json({ category });
