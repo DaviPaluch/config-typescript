80a86e91594b68d482dd85bb0d9b2a829661864c
davi paluch <davipaluch@icloud.com>
2024-06-06 15:04:43 -0300

    Fim de projeto inicial Node com Typescript. Debug & Rotas & Repository & OOP

---
+++ src/repository/CategoriesRepository.ts
@@ -0,0 +1,30 @@
+import exp from "constants";
+import { Category } from "../model/Category";
+
+
+// DTO => Data transfer object
+interface ICreateCategoryDTO {
+  name: string,
+  description: string
+}
+class CategoriesRepositoty {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ description, name }: ICreateCategoryDTO): void { // indicamos que receberemos um objeto do tipo ICreateCategoryDTO e fazemos a desestruturação dele. Depois indicamos que o retorno é VOID.
+    const category = new Category()
+
+    Object.assign(this.categories, {
+      name,
+      description,
+      created_at: new Date()
+    })
+
+    this.categories.push(category)
+  }
+}
+
+export { CategoriesRepositoty };
